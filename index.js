ff4f6e5ff567df9b890d054f6cc86b6e8a2c829d
Titus Wormer <tituswormer@gmail.com>
2016-11-20 09:51:12 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,55 +1,23 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:visit-children
- * @fileoverview Unist utility to visit direct children of a parent.
- */
-
 'use strict';
 
-/* eslint-env commonjs */
-
-/**
- * Visitor for children of `parent`.
- *
- * @typedef visitChildren~callback
- * @param {Node} child - Current iteration;
- * @param {number} index - Position of `child` in `parent`;
- * @param {Node} parent - Parent node of `child`.
- */
-
-/**
- * Function invoking a bound `fn` for each child of `parent`.
- *
- * @typedef visitChildren~visitor
- * @param {Node} parent - Node with children.
- * @throws {Error} - When not given a parent node.
- */
-
-/**
- * Turns `callback` into a child-visitor accepting a parent.
- *
- * @param {visitChildren~callback} callback - Function to wrap.
- * @return {visitChildren~visitor} - Wrapped `fn`.
- */
-function visitorFactory(callback) {
-    return function (parent) {
-        var index = -1;
-        var children = parent && parent.children;
-
-        if (!children) {
-            throw new Error('Missing children in `parent` for `visitor`');
-        }
-
-        while (++index in children) {
-            callback(children[index], index, parent);
-        }
-    };
-}
-
-/*
- * Expose.
- */
-
+/* Expose. */
 module.exports = visitorFactory;
+
+/* Turns `callback` into a child-visitor accepting a parent. */
+function visitorFactory(callback) {
+  return visitor;
+
+  /* Visit `parent`, invoking `callback` for each child. */
+  function visitor(parent) {
+    var index = -1;
+    var children = parent && parent.children;
+
+    if (!children) {
+      throw new Error('Missing children in `parent` for `visitor`');
+    }
+
+    while (++index in children) {
+      callback(children[index], index, parent);
+    }
+  }
+}
--- test.js
+++ test.js
@@ -1,81 +1,61 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module unist:util:visit-children
- * @fileoverview Test suite for `unist-util-visit-children`.
- */
-
 'use strict';
 
-/* eslint-env node, mocha */
+/* eslint-env mocha */
 
-/*
- * Dependencies.
- */
-
+/* Dependencies. */
 var assert = require('assert');
 var visitChildren = require('./');
 
-/*
- * Methods.
- */
+var noop = Function.prototype;
 
-var throws = assert.throws;
-var equal = assert.strictEqual;
-
-/**
- * No-op.
- */
-function noop() {}
-
-noop();
-
-/*
- * Tests.
- */
-
+/* Tests. */
 describe('visitChildren()', function () {
-    it('should throw when no `parent` is given', function () {
-        throws(function () {
-            visitChildren(noop)();
-        }, /Missing children in `parent`/);
+  it('should throw when no `parent` is given', function () {
+    assert.throws(
+      function () {
+        visitChildren(noop)();
+      },
+      /Missing children in `parent`/
+    );
 
-        throws(function () {
-            visitChildren(noop)({});
-        }, /Missing children in `parent`/);
+    assert.throws(
+      function () {
+        visitChildren(noop)({});
+      },
+      /Missing children in `parent`/
+    );
+  });
+
+  it('should invoke `fn` for each child in `parent`', function () {
+    var values = [0, 1, 2, 3];
+    var context = {};
+    var n = -1;
+
+    context.children = values;
+
+    visitChildren(function (value, index, parent) {
+      n++;
+      assert.strictEqual(value, values[n]);
+      assert.strictEqual(index, n);
+      assert.strictEqual(parent, context);
+    })(context);
+  });
+
+  it('should work when new children are added', function () {
+    var values = [0, 1, 2, 3, 4, 5, 6];
+    var n = -1;
+
+    visitChildren(function (value, index, parent) {
+      n++;
+
+      if (index < 3) {
+        parent.children.push(parent.children.length);
+      }
+
+      assert.strictEqual(value, values[n]);
+      assert.strictEqual(index, values[n]);
+    })({
+      children: [0, 1, 2, 3]
     });
-
-    it('should invoke `fn` for each child in `parent`', function () {
-        var values = [0, 1, 2, 3];
-        var context = {};
-        var n = -1;
-
-        context.children = values;
-
-        visitChildren(function (value, index, parent) {
-            n++;
-            equal(value, values[n]);
-            equal(index, n);
-            equal(parent, context);
-        })(context);
-    });
-
-    it('should work when new children are added', function () {
-        var values = [0, 1, 2, 3, 4, 5, 6];
-        var n = -1;
-
-        visitChildren(function (value, index, parent) {
-            n++;
-
-            if (index < 3) {
-                parent.children.push(parent.children.length);
-            }
-
-            equal(value, values[n]);
-            equal(index, values[n]);
-        })({
-            'children': [0, 1, 2, 3]
-        });
-    });
+  });
 });
